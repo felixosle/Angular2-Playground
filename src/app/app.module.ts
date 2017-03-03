0003d614af9e73bda3a466bcbba171381a38e6a7
Felix Martinez <maroslfe@jcyl.es>
2017-03-03 10:05:51 +0100

    componentes de cabecera, menu y pie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PieComponent } from './shared/layout/pie/pie.component';
+import { CabeceraComponent } from './shared/layout/cabecera/cabecera.component';
+import { MenuComponent } from './shared/layout/menu/menu.component';
+import { HomeComponent } from './shared/layout/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent,
+    CabeceraComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
